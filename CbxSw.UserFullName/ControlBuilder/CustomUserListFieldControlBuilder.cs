102cccef8fede2dc3d305f7e3babb25c0338d870
Nik De Clercq <nik@checkboxsoftware.net>
2017-01-24 23:46:37 +0100

    Added a CustomUserListFieldControlBuilder that allows users to search other users using the field UserFullName

---
+++ CbxSw.UserFullName/ControlBuilder/CustomUserListFieldControlBuilder.cs
@@ -0,0 +1,20 @@
+﻿using Adam.Web.Extensions.UI.Controls;
+using Adam.Web.UI.Controls;
+
+namespace CbxSw.UserFullName.ControlBuilder
+{
+	/// <summary>
+	/// This <see cref="FieldControlBuilder"/> allows users to search on the field <see cref="UserFullNameFieldName"/>. 
+	/// </summary>
+	public class CustomUserListFieldControlBuilder : ExtendedUserListFieldControlBuilder
+	{
+		public const string UserFullNameFieldName = "UserFullName";
+
+		protected override ListEditorBase CreateListEditor(FieldControlBuildInfo buildInfo)
+		{
+			var editor = (SearchListEditor)base.CreateListEditor(buildInfo);
+			editor.SearchTemplate = $"fieldname({UserFullNameFieldName})=? or name=?";
+			return editor;
+		}
+	}
+}
---
+++ CbxSw.UserFullName/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Adam.Web.Studio;
+using Adam.Web.Studio.Extensibility;
+using Adam.Web.UI.Controls;
+using CbxSw.UserFullName.ControlBuilder;
+
+namespace CbxSw.UserFullName
+{
+	/// <summary>
+	/// A class that we will configure in the Web.Config of ConfigStudio, telling ADAM to call <see cref="Configure"/> when the studio starts.
+	/// </summary>
+	/// <remarks>
+	/// To get the extension to work, open the web.config and find the section: /configuration/adam.web.studio.
+	/// Then add the following code:
+	/// <code>
+	///   &lt;studioExtensions&gt;
+	///	    &lt;providers&gt;
+	///	      &lt;add type = "CbxSw.UserFullName.Startup, CbxSw.UserFullName" /&gt;
+	///     &lt;/providers&gt;
+	///   &lt;/studioExtensions&gt;
+	/// </code>
+	/// </remarks>
+	public class Startup : StudioExtension
+	{
+		private static readonly object _lock = new object();
+		private static bool _hasStarted = false;
+
+		public override void Configure(HttpStudioApplication application)
+		{
+			if (ShouldRunStartup())
+			{
+				RunsOnceAtStartup();
+			}
+		}
+
+		private static void RunsOnceAtStartup()
+		{
+			FieldControlBuilders.RegisterBuilder(new CustomUserListFieldControlBuilder());
+		}
+
+		private static bool ShouldRunStartup()
+		{
+			lock (_lock) // making sure only one thread can run the startup code at a time
+			{
+				if (_hasStarted) // if startup code has ran before: no need to run it again
+				{
+					return false;
+				}
+				_hasStarted = true;
+				return true;
+			}
+		}
+	}
+}
